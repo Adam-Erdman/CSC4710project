2d387061f96e723df3d9d3b01d178467b5b12307
Adam-Erdman <dz6416@wayne.edu>
2020-02-21 19:04:50 -0500

    Changed people.java for project

--- src/People.java
+++ src/People.java
@@ -1,58 +1,87 @@
 
 
 public class People {
-    protected int id;
-    protected String name;
-    protected String address;
-    protected String status;
+   // protected int id;
+    protected String username;
+    protected String userpassword;
+    protected String firstname;
+    protected String lastname;
+    protected String emailaddress;
  
     public People() {
     }
+    
+    public People(String username, String userpassword, String firstname, String lastname, String emailaddress) {
+      this.username = username;
+      this.userpassword = userpassword;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.emailaddress = emailaddress;
+      
+  }
  
-    public People(int id) {
-        this.id = id;
-    }
+//    public People(int id) {
+//        this.id = id;
+//    }
+// 
+//    public People(int id, String name, String address, String status) {
+//        this(name, address, status);
+//        this.id = id;
+//    }
+//     
+//    public People(String name, String address, String status) {
+//        this.name = name;
+//        this.address = address;
+//        this.status = status;
+//    }
  
-    public People(int id, String name, String address, String status) {
-        this(name, address, status);
-        this.id = id;
-    }
-     
-    public People(String name, String address, String status) {
-        this.name = name;
-        this.address = address;
-        this.status = status;
-    }
+//    public int getId() {
+//        return id;
+//    }
+// 
+//    public void setId(int id) {
+//        this.id = id;
+//    }
  
-    public int getId() {
-        return id;
-    }
- 
-    public void setId(int id) {
-        this.id = id;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public void setName(String name) {
-        this.name = name;
-    }
- 
-    public String getAddress() {
-        return address;
-    }
- 
-    public void setAddress(String address) {
-        this.address = address;
+    public String getUserName() {
+        return username;
     }
  
-    public String getStatus() {
-        return status;
+    public void setUserName(String username) {
+        this.username = username;
     }
  
-    public void setStatus(String status) {
-        this.status = status;
+    public String getUserPassWord() {
+        return userpassword;
     }
+ 
+    public void setUserPassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+    
+    public String getFirstName() {
+        return firstname;
+    }
+ 
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    public String getLastName() {
+        return lastname;
+    }
+ 
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    public String getEmailAddress() {
+        return emailaddress;
+    }
+ 
+    public void setEmailAddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+ 
+   
 }
